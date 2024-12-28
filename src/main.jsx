3fff84958450f322c701d4810eb9d00907bdedab
madhava <madhavavukkum@gmail.com>
2024-12-28 19:41:33 +0530

    Updated About, Footer, Home components and added Contact, Github directories

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,22 @@ import Layout from './Layout.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import User from "./components/User/User.jsx"
+import Github , {githubInfoLoader} from './components/Github/Github.jsx';
+import Contact from './components/Contact/Contact.jsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />} >
           <Route path="" element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="user" element={<User/>}/>
+          <Route path="contact" element={<Contact/>} />
+          <Route path="user" element={<User/>}>
+            <Route path=":usderid" element={<User/>}/>
+          </Route>
+          <Route
+          loader = {githubInfoLoader}
+          path="github" element={<Github/>} />
+          <Route path="*" element={<div>404</div>} />
         </Route>
     )
 );
